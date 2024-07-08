fec6a3ca3d6241cb267f80a5e3264e10fc004881
Jensfrank <everett7623@gmail.com>
2024-07-08 15:32:09 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,10 +9,17 @@ npm run docs:build
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
+
 git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:everett7623/nav.git master:gh-pages
 
 cd -
