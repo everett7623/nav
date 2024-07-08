0177c55d15628a628f4770f7d91015dfef9b5db3
Jensfrank <everett7623@gmail.com>
2024-07-08 16:06:52 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,26 +1,19 @@
 module.exports = {
-  title: '导航站',
-  description: '一个简单的导航站',
+  title: 'MyNAV 导航站',
+  description: '一个简单的导航站示例',
   themeConfig: {
-    nav: [
-      { text: '首页', link: '/' },
-      { text: '分类1', link: '/category1/' },
-      { text: '分类2', link: '/category2/' },
-      { text: '管理', link: '/admin/' }
-    ],
     sidebar: {
       '/category1/': [
         '',
-        'sites'
+        'sites'  // 对应 category1/sites.md
       ],
       '/category2/': [
         '',
-        'sites'
+        'sites'  // 对应 category2/sites.md
+      ],
+      '/admin/': [
+        ''
       ]
     }
-  },
-  plugins: [
-    '@vuepress/plugin-back-to-top',
-    '@vuepress/plugin-medium-zoom',
-  ]
+  }
 }
