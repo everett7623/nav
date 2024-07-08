aac2c3c26baefe1d838436eea9cd7db31d7db83c
Jensfrank <everett7623@gmail.com>
2024-07-08 15:24:52 +0800

    Create NavLinks.vue

---
+++ src/components/NavLinks.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="nav-links">
+    <a v-for="link in links" :key="link.url" :href="link.url">
+      {{ link.title }}
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['links']
+}
+</script>
