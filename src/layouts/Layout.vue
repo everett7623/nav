70364e11a7bc527455dcafd828bbcf5590820db1
Jensfrank <everett7623@gmail.com>
2024-07-08 15:24:34 +0800

    Create Layout.vue

---
+++ src/layouts/Layout.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="layout">
+    <div class="sidebar">
+      <Sidebar />
+    </div>
+    <div class="content">
+      <Content />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.layout {
+  display: flex;
+}
+.sidebar {
+  width: 250px;
+}
+.content {
+  flex: 1;
+}
+</style>
